d22f059820082cafccd2b70d2f7ada1e2846a7de
latestbob <edidiongbobson@gmail.com>
2023-11-18 11:36:58 -0800

    added status

--- server.js
+++ server.js
@@ -67,16 +67,21 @@ app.post('/api/send', async (req, res) => {
   };
 
 
+
   try {
     let info = await transport.sendMail(mailOptions);
     console.log('Message sent: %s', info.messageId);
-  } catch (error) {
+    res.status(200).json({ message: 'Email sent successfully' });
+
+    
+} catch (error) {
     console.error('Error sending email:', error);
-  }
+    res.status(500).json({ message: 'Error sending email', error: error.message });
+}
 
 
 
-res.status(200).json({ message: 'Working' + type });
+
 
 
 
